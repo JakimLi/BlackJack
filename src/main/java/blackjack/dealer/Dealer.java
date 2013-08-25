fb657de8e66b427015a2dcac59da1d9fb27a4477
JakimLi <ljking7002@gmail.com>
2013-08-25 16:21:32 +0800

    The dealer can pick a card randomly, without repeated cards.

--- src/main/java/blackjack/dealer/Dealer.java
+++ src/main/java/blackjack/dealer/Dealer.java
@@ -1,12 +1,20 @@
 package blackjack.dealer;
 
-public class Dealer {
+import java.util.Random;
 
-    private String[] values = new String[]{"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+import static org.apache.commons.lang.ArrayUtils.removeElement;
+
+public class Dealer {
+    private final String[] values = new String[]{"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+
     private Card[] cards = new Card[52];
+    private Random randomGenerator;
+
+    public Dealer() {
+        getCards();
+    }
 
     public Card[] getCards() {
-
         int i = 0;
         for (String value : values) {
             cards[i++] = new Card(value, "Spades");
@@ -15,5 +23,19 @@ public class Dealer {
             cards[i++] = new Card(value, "Diamonds");
         }
         return cards;
+    }
+
+    public void setRandomGenerator(Random randomGenerator) {
+        this.randomGenerator = randomGenerator;
+    }
+
+    public Card pickACard() {
+        if( cards == null || cards.length <= 0) {
+            throw new RuntimeException("No cards left in dealer's hands");
+        }
+        int random = randomGenerator.nextInt(cards.length);
+        Card card = cards[random];
+        cards = (Card[]) removeElement(cards, card);
+        return card;
     }
 }
--- src/test/java/blackjack/dealer/DealerTest.java
+++ src/test/java/blackjack/dealer/DealerTest.java
@@ -2,9 +2,16 @@ package blackjack.dealer;
 
 import org.junit.Test;
 
+import java.util.Random;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.arrayWithSize;
 import static org.hamcrest.Matchers.hasItemInArray;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class DealerTest {
 
@@ -23,11 +30,60 @@ public class DealerTest {
 
         assertThat(cards, arrayWithSize(52));
 
-        for (String value: values){
+        for (String value : values) {
             assertThat(cards, hasItemInArray(new Card(value, "Spades")));
             assertThat(cards, hasItemInArray(new Card(value, "Hearts")));
             assertThat(cards, hasItemInArray(new Card(value, "Clubs")));
             assertThat(cards, hasItemInArray(new Card(value, "Diamonds")));
         }
+    }
+
+    @Test
+    public void should_pick_one_card_randomly_from_the_deck_of_card() throws Exception {
+        //given
+        Random randomGenerator = mock(Random.class);
+        when(randomGenerator.nextInt(anyInt())).thenReturn(0);
+
+        Dealer dealer = new Dealer();
+        dealer.setRandomGenerator(randomGenerator);
+
+        //when
+        Card card = dealer.pickACard();
+
+        //then
+        assertThat(card, is(new Card("Ace", "Spades")));
+    }
+
+    @Test
+    public void should_not_pick_card_already_picked() throws Exception {
+        //given
+        Dealer dealer = new Dealer();
+        Random randomGenerator = mock(Random.class);
+        when(randomGenerator.nextInt(anyInt())).thenReturn(0);
+        dealer.setRandomGenerator(randomGenerator);
+
+        //when
+        Card firstCard = dealer.pickACard();
+        Card secondCard = dealer.pickACard();
+        //then
+
+        assertThat(firstCard, is(new Card("Ace", "Spades")));
+        assertThat(firstCard, not(secondCard));
+    }
+
+    @Test (expected = RuntimeException.class)
+    public void should_error_after_all_cards_were_picked() throws Exception {
+        //given
+        Dealer dealer = new Dealer();
+        Random randomGenerator = mock(Random.class);
+        when(randomGenerator.nextInt(anyInt())).thenReturn(0);
+        dealer.setRandomGenerator(randomGenerator);
+
+        //when
+        for (int i = 0; i < 52; i++) {
+            dealer.pickACard();
+        }
+
+        dealer.pickACard();
     }
 }
