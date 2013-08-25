30ee6fef971e6b8d278a47ad3185c1b8ac0c8a5d
JakimLi <ljking7002@gmail.com>
2013-08-25 16:31:46 +0800

    Refactor code

--- src/main/java/blackjack/dealer/Dealer.java
+++ src/main/java/blackjack/dealer/Dealer.java
@@ -5,7 +5,7 @@ import java.util.Random;
 import static org.apache.commons.lang.ArrayUtils.removeElement;
 
 public class Dealer {
-    private final String[] values = new String[]{"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+    public static final String[] CARD_VALUES = new String[]{"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
 
     private Card[] cards = new Card[52];
     private Random randomGenerator;
@@ -16,13 +16,18 @@ public class Dealer {
 
     public Card[] getCards() {
         int i = 0;
-        for (String value : values) {
-            cards[i++] = new Card(value, "Spades");
-            cards[i++] = new Card(value, "Hearts");
-            cards[i++] = new Card(value, "Clubs");
-            cards[i++] = new Card(value, "Diamonds");
+        for (String value : CARD_VALUES) {
+            i = fillCardsBySuits(i, value);
         }
         return cards;
+    }
+
+    private int fillCardsBySuits(int i, String value) {
+        cards[i++] = new Card(value, "Spades");
+        cards[i++] = new Card(value, "Hearts");
+        cards[i++] = new Card(value, "Clubs");
+        cards[i++] = new Card(value, "Diamonds");
+        return i;
     }
 
     public void setRandomGenerator(Random randomGenerator) {
@@ -30,11 +35,10 @@ public class Dealer {
     }
 
     public Card pickACard() {
-        if( cards == null || cards.length <= 0) {
+        if (cards == null || cards.length <= 0) {
             throw new RuntimeException("No cards left in dealer's hands");
         }
-        int random = randomGenerator.nextInt(cards.length);
-        Card card = cards[random];
+        Card card = cards[randomGenerator.nextInt(cards.length)];
         cards = (Card[]) removeElement(cards, card);
         return card;
     }
--- src/test/java/blackjack/dealer/DealerTest.java
+++ src/test/java/blackjack/dealer/DealerTest.java
@@ -1,9 +1,11 @@
 package blackjack.dealer;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Random;
 
+import static blackjack.dealer.Dealer.CARD_VALUES;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -15,14 +17,18 @@ import static org.mockito.Mockito.when;
 
 public class DealerTest {
 
-    private String[] values = new String[]{"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+    private Dealer dealer;
+
+    @Before
+    public void setUp() throws Exception {
+        dealer = new Dealer();
+        Random randomGenerator = mock(Random.class);
+        when(randomGenerator.nextInt(anyInt())).thenReturn(0);
+        dealer.setRandomGenerator(randomGenerator);
+    }
 
     @Test
     public void dealer_should_have_one_deck_of_cards() throws Exception {
-        //given
-        Dealer dealer = new Dealer();
-
-        //then
         assertOneDeckOfCards(dealer.getCards());
     }
 
@@ -30,7 +36,7 @@ public class DealerTest {
 
         assertThat(cards, arrayWithSize(52));
 
-        for (String value : values) {
+        for (String value : CARD_VALUES) {
             assertThat(cards, hasItemInArray(new Card(value, "Spades")));
             assertThat(cards, hasItemInArray(new Card(value, "Hearts")));
             assertThat(cards, hasItemInArray(new Card(value, "Clubs")));
@@ -40,13 +46,6 @@ public class DealerTest {
 
     @Test
     public void should_pick_one_card_randomly_from_the_deck_of_card() throws Exception {
-        //given
-        Random randomGenerator = mock(Random.class);
-        when(randomGenerator.nextInt(anyInt())).thenReturn(0);
-
-        Dealer dealer = new Dealer();
-        dealer.setRandomGenerator(randomGenerator);
-
         //when
         Card card = dealer.pickACard();
 
@@ -56,29 +55,17 @@ public class DealerTest {
 
     @Test
     public void should_not_pick_card_already_picked() throws Exception {
-        //given
-        Dealer dealer = new Dealer();
-        Random randomGenerator = mock(Random.class);
-        when(randomGenerator.nextInt(anyInt())).thenReturn(0);
-        dealer.setRandomGenerator(randomGenerator);
-
         //when
         Card firstCard = dealer.pickACard();
         Card secondCard = dealer.pickACard();
-        //then
 
+        //then
         assertThat(firstCard, is(new Card("Ace", "Spades")));
         assertThat(firstCard, not(secondCard));
     }
 
-    @Test (expected = RuntimeException.class)
+    @Test(expected = RuntimeException.class)
     public void should_error_after_all_cards_were_picked() throws Exception {
-        //given
-        Dealer dealer = new Dealer();
-        Random randomGenerator = mock(Random.class);
-        when(randomGenerator.nextInt(anyInt())).thenReturn(0);
-        dealer.setRandomGenerator(randomGenerator);
-
         //when
         for (int i = 0; i < 52; i++) {
             dealer.pickACard();
